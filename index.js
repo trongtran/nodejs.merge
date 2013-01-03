cea47a28e3db0eb88868aa094f92cf0e2f69c4ea
trongtran <trongtran.javadev@gmail.com>
2013-01-03 15:37:10 +0700

    Update index.js
    Refactor and reformat code

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
+/**
+ * The helper function used to merge multiple objects into one object.
+ * 
+ * @return {Object} New object.
+ */
 module.exports = function() {
 
-	var x = Array.prototype.slice.call(arguments),
-		y = x.shift();
+  var x = Array.prototype.slice.call(arguments),
+      y = x.shift();
 
-	x.forEach(function(z) {
+  x.forEach(function(z) {
+    if (typeof z == 'object')
 
-		if (typeof z == 'object')
-
-			Object.getOwnPropertyNames(z).forEach(function(w) {
-
-				Object.defineProperty(y, w, Object.getOwnPropertyDescriptor(z, w));
-			
-			});
-		
-	});
-	
-	return y;
-	
+      Object.getOwnPropertyNames(z).forEach(function(w) {
+      	
+        Object.defineProperty(y, w, Object.getOwnPropertyDescriptor(z, w));
+      });
+    
+  });
+  
+  // Return new object
+  return y;
 };
